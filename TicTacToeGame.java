d8231a756de72b099edd5f6c6757a272010a2f98
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-12-24 10:37:28 +0530

    Added:Ability to allow player

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -6,7 +6,17 @@ public class TicTacToeGame
 {
 	public static void main(String[] args)
 	{
+		int index,randomCheck = 0,toss=0;
 		char[] board=createBoard();
+		play();
+		if(randomCheck==toss)
+		{
+			System.out.println("Player will play");
+		}
+		else
+		{
+			System.out.println("Computer will play");
+		}
 	}
 
 	private static char[] createBoard()
@@ -17,5 +27,12 @@ public class TicTacToeGame
 			board[i]=' ';//Initializing the board
 		}
 		return board;
+	}
+	private static int play()
+	{
+		int toss=1;
+		double rndomCheck=Math.floor(Math.random()*10)%2;
+		int randomCheck = 0;
+		return randomCheck;
 	}
 }
