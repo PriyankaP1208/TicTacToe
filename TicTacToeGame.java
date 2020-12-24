567b2485bf79af1f5b86e8d7a57e52d346b250b2
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-12-24 10:31:12 +0530

    Added:Create game board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -6,6 +6,16 @@ public class TicTacToeGame
 {
 	public static void main(String[] args)
 	{
-		
+		char[] board=createBoard();
+	}
+
+	private static char[] createBoard()
+	{
+		char[] board=new char[10];
+		for(int i=0;i<board.length;i++)
+		{
+			board[i]=' ';//Initializing the board
+		}
+		return board;
 	}
 }
